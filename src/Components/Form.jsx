2584fc27b2fbe0908b379f97ce3afdade1da5944
NahuMeza <nahuelgmeza@gmail.com>
2023-09-19 21:21:21 -0300

    add form validations

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,9 +1,30 @@
+import { useForm } from "./Hooks/useForm"
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
+  const {handleSubmit, nameValid, emailValid, error} = useForm();
 
   return (
     <div>
-      <form></form>
+      <form noValidate onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input 
+          style={{ border: !nameValid && "1px solid red" }}
+          type="text" 
+          id="name"
+          name="name"
+          placeholder="Name"
+        />
+        <label htmlFor="email">Email</label>
+        <input 
+          style={{ border: !emailValid && "1px solid red" }} 
+          type="email" 
+          id="email" 
+          name="email"
+          placeholder="Email"
+        />
+        <button type="submit">Submit</button>
+      </form>
+        {error && <p>{error}</p>}
     </div>
   );
 };
---
+++ src/Components/Hooks/useForm.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export const useForm = () => {
+      const errorStr = {
+    name: "Name must be at least 5 characters long",
+    email: "Email must be a valid email",
+  }
+  const [emailValid, setEmailValid] = useState(true);
+  const [nameValid, setNameValid] = useState(true);
+  const [error, setError] = useState(false);
+
+  function validarEmail(email) {
+    var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return regex.test(email);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.name.value);
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+
+    if(name.length < 5){
+      setNameValid(false);
+      setError(errorStr.name);
+      return;
+    }
+    if(!validarEmail(email)){
+      setEmailValid(false);
+      setError(errorStr.email);
+      return;
+    }
+    setNameValid(true);
+    setEmailValid(true);
+    setError(false);
+  }
+
+    return {handleSubmit, nameValid, emailValid, error};
+}
